7af1d1d384950f8ac1ec154b34033aae79fcacf2
Mevlüt Sönmez <sonmezcan1997@gmail.com>
2024-10-03 10:41:11 +0300

    ios calculator

---
+++ Calculator-iOS/Controller/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Calculator-iOS
+//
+//  Created by can on 3.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var brain = Brain()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+        brain.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+ 
+            if let result = brain.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+
+}
